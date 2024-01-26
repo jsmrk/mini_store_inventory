3d6c2013022753d69fc722eb0ab6ec84bc7b4354
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2024-01-27 05:33:08 +0800

    add product button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -5,6 +5,47 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color?>(
+          const Color(0xFFF2F2F2),
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            side: const BorderSide(width: 1),
+            borderRadius: BorderRadius.circular(11), // Softer corners
+          ),
+        ),
+        minimumSize: MaterialStateProperty.all(
+          const Size(
+            125,
+            35,
+          ),
+        ), // Wider for tapability
+        textStyle: MaterialStateProperty.all(
+          const TextStyle(
+            fontSize: 15, // Slightly larger text
+            fontWeight: FontWeight.w600, // Bolder text
+          ),
+        ),
+        elevation: MaterialStateProperty.all(0),
+        overlayColor: MaterialStateProperty.all<Color?>(
+          Colors.grey[300],
+        ),
+      ),
+      onPressed: () {},
+      child: const Row(
+        children: [
+          Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
+          Text(
+            ' Add Product',
+            style: TextStyle(color: Colors.black), // Contrasting text color
+          ),
+        ],
+      ),
+    );
   }
 }
