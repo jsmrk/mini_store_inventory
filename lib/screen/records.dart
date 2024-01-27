95a06107789ba2b92dd91d1b214749301dff1593
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2024-01-27 10:29:38 +0800

    Records UI done

--- lib/screen/records.dart
+++ lib/screen/records.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/custom_appbar.dart';
+import '../widgets/product_container.dart';
 
 class Records extends StatelessWidget {
   const Records({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
-      backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Container(
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        height: double.infinity,
+        width: double.infinity,
+        child: Container(
+          alignment: Alignment.topLeft,
+          child: const DefaultTabController(
+            length: 5,
+            child: TabBar(
+              indicatorColor: Color(0xFF33186B),
+              labelColor: Color(0xFF33186B),
+              labelPadding: EdgeInsets.all(9),
+              indicatorWeight: 5,
+              indicatorSize: TabBarIndicatorSize.tab,
+              tabs: [
+                Text(
+                  'Sales',
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Stocks',
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
